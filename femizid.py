ecd9f63f53f22cb0f6e55a610bdd403b929c84c3
christinegotth <christine.gotth@gmail.com>
2019-11-20 14:27:17 +0530

    fix parser

--- femizid.py
+++ femizid.py
@@ -6,8 +6,6 @@ import pandas as pd
 URL = "http://www.onebillionrising.de/femizid-opfer-meldungen-2019/"
 r = requests.get(URL) 
   
-soup = BeautifulSoup(r.content, 'html5lib') 
+soup = BeautifulSoup(r.content, 'html.parser') 
 
-cases = re.search("(Reine Quellensammlung ohne Garantie auf Richtigkeit oder Vollständigkeit)", soup)
-
-print(cases.prettify())
+print(soup.prettify())
