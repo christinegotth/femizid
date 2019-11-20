de8a6aa020733b55c3e9323249e79afa01821b9d
christinegotth <christine.gotth@gmail.com>
2019-11-20 15:49:52 +0530

    fixed json output

--- femizid.py
+++ femizid.py
@@ -1,11 +1,21 @@
 import requests 
 from bs4 import BeautifulSoup
 import re 
+import json
 import pandas as pd
+
+#var maplistScriptParamsKo
   
-URL = "http://www.onebillionrising.de/femizid-opfer-meldungen-2019/"
-r = requests.get(URL) 
+data = "http://www.onebillionrising.de/femizid-opfer-meldungen-2019/"
+r = requests.get(data) 
   
 soup = BeautifulSoup(r.content, 'html.parser') 
 
-print(soup.prettify())
+pattern = re.compile(r"var maplistScriptParamsKo\s+=\s+(\{.*?\});\n")
+script = soup.find("script", text=pattern)
+
+data = pattern.search(script.text).group(1)
+
+data = json.loads(data)
+
+print(data)
